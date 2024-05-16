259eae5c2b086e3c8a283a7e61ac6d7d51dfb64d
Kajo <kajo@home>
2024-05-16 17:42:36 +0100

    .

--- source/minishell.c
+++ source/minishell.c
@@ -9,10 +9,8 @@ void	exit_minishell(t_ms *s)
 
 
 
-int	init_minishell(t_ms *s, int ac, char **av, char **ep)
+int	init_minishell(t_ms *s, char **ep)
 {
-	(void)ac;
-	(void)av;
 	(void)ep;
 
 	s->prompt = "write here ->";	// v zero
@@ -22,24 +20,66 @@ int	init_minishell(t_ms *s, int ac, char **av, char **ep)
 
 	return (true);
 }
-// lexer -> parser -> expander -> executor
-int main(int argc, char *argv[], char *envp[])
+
+void	handle_signal()
+{
+	// t_signal	signal;
+
+	if (signal(SIGINT, sig_usr))
+		perror("can't catch SIGINT");
+	else if (signal(SIGTERM, sig_usr))
+		perror("can't catch SIGTERM");
+	else if (signal(SIGQUIT, sig_usr))
+		perror("can't catch SIGQUIT");
+	else
+		return ;
+		// exit(0);
+}
+
+ /* argument is signal number */
+static void sig_usr(int signo)
+{
+	if (signo == SIGINT)
+		printf("received SIGINT\n");
+	else if (signo == SIGTERM)
+		printf("received SIGTERM\n");
+	else if (signo == SIGQUIT)
+		printf("received SIGQUIT\n");
+	else
+		exit(0);
+}
+
+void	minishell(char **envp)
 {
 	t_ms	s;
 	char	*input;
 
-	if (!init_minishell(&s, argc, argv, envp))
+	(void)envp;
+
+	if (!init_minishell(&s, envp))
 		exit_minishell(&s);
-	printf("printf do Carlos\n");
-	printf("minishell");
 	while (true)
 	{
+		handle_signal();
+		input = readline(s.prompt);
 		// lexer
 		// parser
 		// expander
 		// executor
-		input = readline(s.prompt);
 		ft_dprintf(STDOUT_FILENO, "%s\n", input);
 	}
+
+}
+
+// lexer -> parser -> expander -> executor
+int main(int argc, char *argv[], char *envp[])
+{
+
+	(void)argv;
+
+	if (argc != 1)
+		return (0);
+	minishell(envp);
+
 	return (0);
 }
