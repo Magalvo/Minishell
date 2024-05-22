bdf24536dc1cffcd48e97971f4d692199ee0029c
kajo@home <kajo@home>
2024-05-22 16:28:36 +0100

    nanoshell??

--- source/minishell.c
+++ source/minishell.c
@@ -12,6 +12,8 @@ void exit_minishell(t_ms *s, char *msg)
 	if (msg)
 		ft_putstr_fd(msg, 1);
 	(void)s;
+	ft_free_dptr(s->paths);
+	ft_free_dptr(s->cmd_temp);
 	exit(0);
 }
 
@@ -34,18 +36,32 @@ int init_minishell(t_ms *s, char **ep)
 // STATUS: signal caught, ctrl+\ shouldn't print to stdout
 void minishell(char **envp)
 {
-	t_ms s;
-	char *input;
+	t_ms	s;
+	char	*input;
+	int		i;
 
 	if (!init_minishell(&s, envp))
 		exit_minishell(&s, NULL);
 	while (true)
 	{
+		i = 0;
 		check_signal(&s);
 		input = readline(s.prompt);
 		if (input == NULL && s.modal == MAIN)
 			exit_minishell(&s, "exit\n");
 		split_input(&s, input);
+		// while (s.paths[i])
+		// {
+		// 	ft_putendl_fd(s.paths[i], 1);
+		// 	i++;
+		// }
+		// i = 0;
+		// while (s.cmd_temp[i])
+		// {
+		// 	ft_putendl_fd(s.cmd_temp[i], 1);
+		// 	i++;
+		// }
+
 		// exec_input(&s); // ! make this
 		// todo
 		/* lexer
