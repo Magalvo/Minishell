806c71926de793fc8e59bafbf367b0bd14459d8c
Diogo Jose De Magalhaes Calvo Pires Dos Santos <dde-maga@c2r8s1.42lisboa.com>
2024-05-22 16:28:33 +0100

    more builtins

--- source/builtins/export_cmd.c
+++ source/builtins/export_cmd.c
@@ -6,7 +6,7 @@
 /*   By: dde-maga <dde-maga@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/20 14:58:59 by dde-maga          #+#    #+#             */
-/*   Updated: 2024/05/22 15:56:05 by dde-maga         ###   ########.fr       */
+/*   Updated: 2024/05/22 16:15:16 by dde-maga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,13 +23,16 @@ int	export_cmd(t_ms *s, t_cmd *cmd)
 		{
 			if (env->key == cmd->cmd_args[0] && cmd->cmd_args[1])
 				env->value = cmd->cmd_args[1];
-			else if ()
+			else if (cmd->cmd_args[1] || env->value == NULL)
 			{
-				
+				//*add_env_node();
+				printf("exporting...");
+				break;
 			}
 			env = env->next;
 		}
-
+		else
+			return(error_msg("Export need Args!"));
 	}
 	return (1);
 }
--- source/builtins/unset_cmd.c
+++ source/builtins/unset_cmd.c
@@ -6,15 +6,27 @@
 /*   By: dde-maga <dde-maga@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/20 14:58:50 by dde-maga          #+#    #+#             */
-/*   Updated: 2024/05/21 16:59:51 by dde-maga         ###   ########.fr       */
+/*   Updated: 2024/05/22 16:21:37 by dde-maga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-int unset_cmd(t_ms *mini)
+int	unset_cmd(t_ms *s, t_cmd *cmd)
 {
-	(void)mini;
-	ft_sprintf(1, "unset not implemented");
+	t_env	*env;
+	
+	env = s->env;
+	while (env)
+	{
+		if (cmd->cmd_args[1] != NULL)
+		{
+			if (cmd->cmd_args[1] == env->key)
+				env.key = cmd->cmd_args[1];
+			env = env->next;
+		}
+		else
+			return(error_msg("Unset need Args!"));
+	}
 	return (1);
 }
