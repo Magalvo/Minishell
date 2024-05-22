e30e1969b6cc14356152ccec5668052b76d134ad
Diogo Jose De Magalhaes Calvo Pires Dos Santos <dde-maga@c2r8s1.42lisboa.com>
2024-05-22 15:57:28 +0100

    builtins

--- source/builtins/builtins.c
+++ source/builtins/builtins.c
@@ -6,7 +6,7 @@
 /*   By: dde-maga <dde-maga@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/20 14:59:25 by dde-maga          #+#    #+#             */
-/*   Updated: 2024/05/21 17:02:35 by dde-maga         ###   ########.fr       */
+/*   Updated: 2024/05/22 15:50:41 by dde-maga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,20 @@
 //! Executar Builtins no processo pai a nao ser o printenv;
 // ? Talvez possa ser no filho segundo o Mindmap  D:  :')
 
-int	ft_exec_buitltins(t_ms *mini, t_cmd *cmds)
+int	ft_exec_buitltins(t_ms *s, t_cmd *cmds)
 {
 	if (ft_strcmp(cmds->cmd_args[0], "echo") == 0)
 		return (echo_cmd(cmds));
 	if (ft_strcmp(cmds->cmd_args[0], "cd") == 0)
-		return (cd_cmd(mini));
+		return (cd_cmd(s));
 	if (ft_strcmp(cmds->cmd_args[0], "env") == 0)
-		return (env_cmd(mini));
+		return (env_cmd(s));
 	if (ft_strcmp(cmds->cmd_args[0], "pwd") == 0)
-		return (pwd_cmd(mini));
+		return (pwd_cmd(s));
 	if (ft_strcmp(cmds->cmd_args[0], "export") == 0)
-		return (export_cmd(mini));
+		return (export_cmd(s, cmds));
 	if (ft_strcmp(cmds->cmd_args[0], "unset") == 0)
-		return (unset_cmd(mini));
-	exit_cmd(mini);
+		return (unset_cmd(s));
+	exit_cmd(s);
 	return (1);
 }
--- source/builtins/export_cmd.c
+++ source/builtins/export_cmd.c
@@ -6,16 +6,30 @@
 /*   By: dde-maga <dde-maga@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/20 14:58:59 by dde-maga          #+#    #+#             */
-/*   Updated: 2024/05/22 10:24:23 by dde-maga         ###   ########.fr       */
+/*   Updated: 2024/05/22 15:56:05 by dde-maga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-int	export_cmd(t_ms *mini)
+int	export_cmd(t_ms *s, t_cmd *cmd)
 {
+	t_env	*env;
+	
+	env = s->env;
+	while (env)
+	{
+		if (cmd->cmd_args[1] != NULL)
+		{
+			if (env->key == cmd->cmd_args[0] && cmd->cmd_args[1])
+				env->value = cmd->cmd_args[1];
+			else if ()
+			{
+				
+			}
+			env = env->next;
+		}
 
-
-
+	}
 	return (1);
 }
