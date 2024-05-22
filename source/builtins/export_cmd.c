d12d69942c8bc37e871132b8e2d900ed0c647b3d
Diogo Jose De Magalhaes Calvo Pires Dos Santos <dde-maga@c2r8s1.42lisboa.com>
2024-05-22 12:06:50 +0100

    added something to libft

--- source/builtins/export_cmd.c
+++ source/builtins/export_cmd.c
@@ -6,7 +6,7 @@
 /*   By: dde-maga <dde-maga@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/20 14:58:59 by dde-maga          #+#    #+#             */
-/*   Updated: 2024/05/21 16:59:39 by dde-maga         ###   ########.fr       */
+/*   Updated: 2024/05/22 10:24:23 by dde-maga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,8 @@
 
 int	export_cmd(t_minis *mini)
 {
-	(void)mini;
-	ft_sprintf(1, "export not implemented");
+	
+	
+	
 	return (1);
 }
