f174601dbfe1e0108b7a4e3f1394fb2b98cd46d5
kajo@42 <kajo@42>
2024-06-03 19:58:52 +0100

    merge changes

--- source/parser/init_cmd.c
+++ source/parser/init_cmd.c
@@ -1,31 +1,6 @@
 
 #include "../include/minishell.h"
 
-// char whitespace[] = " \t\r\n\v";
-// char symbols[] = "<|>&;()";
-
-// ast_factory(&s, input)
-// {
-
-// }
-
-t_cmd	*parse_input(char *input)
-{
-	// parsecmd(input);
-	// void (*input)
-	return (parsecmd(input));
-}
-
-// int getcmd(char *buf, int nbuf)
-// {
-// 	write(2, "$ ", 2);
-// 	memset(buf, 0, nbuf);
-// 	gets(buf, nbuf);
-// 	if(buf[0] == 0) // EOF
-// 		return -1;
-// 	return 0;
-// }
-
 t_cmd *execcmd(void)
 {
 	t_cmd	*cmd;
@@ -33,7 +8,6 @@ t_cmd *execcmd(void)
 	cmd = malloc(sizeof(*cmd));
 	ft_memset(cmd, 0, sizeof(*cmd));
 	cmd->type = EXEC;
-	// return (t_cmd*)cmd;
 	return (cmd);
 }
 
@@ -49,7 +23,6 @@ t_cmd *redircmd(t_cmd *subcmd, char *file, char *efile, int mode, int fd)
 	cmd->efile = efile;
 	cmd->mode = mode;
 	cmd->fd = fd;
-	// return (t_cmd*)cmd;
 	return (cmd);
 }
 
@@ -62,52 +35,5 @@ t_cmd *pipecmd(t_cmd *left, t_cmd *right)
 	cmd->type = PIPE;
 	cmd->left = left;
 	cmd->right = right;
-	// return (t_cmd*)cmd;
 	return (cmd);
-}
-
-char symbols[] = "<|>&;()";
-
-int gettoken(char **ps, char *es, char **q, char **eq)
-{
-	char *s;
-	int ret;
-
-	s = *ps;
-	while(s < es && strchr(SPACES, *s))
-		s++;
-	if(q)
-		*q = s;
-	ret = *s;
-	switch(*s){
-	case 0:
-		break;
-	case '|':
-	case '(':
-	case ')':
-	case ';':
-	case '&':
-	case '<':
-		s++;
-		break;
-	case '>':
-		s++;
-		if(*s == '>'){
-		ret = '+';
-		s++;
-		}
-		break;
-	default:
-		ret = 'a';
-		while(s < es && !strchr(SPACES, *s) && !strchr(SYMBOLS, *s))
-		s++;
-		break;
-	}
-	if(eq)
-		*eq = s;
-
-	while(s < es && strchr(SPACES, *s))
-		s++;
-	*ps = s;
-	return ret;
 }
---
+++ source/parser/tokens.c
@@ -0,0 +1,49 @@
+
+#include "../include/minishell.h"
+
+// char whitespace[] = " \t\r\n\v";
+// char symbols[] = "<|>&;()";
+
+int gettoken(char **ps, char *es, char **q, char **eq)
+{
+	char *s;
+	int ret;
+
+	s = *ps;
+	while(s < es && strchr(SPACES, *s))
+		s++;
+	if(q)
+		*q = s;
+	ret = *s;
+	switch(*s){
+	case 0:
+		break;
+	case '|':
+	case '(':
+	case ')':
+	case ';':
+	case '&':
+	case '<':
+		s++;
+		break;
+	case '>':
+		s++;
+		if(*s == '>'){
+		ret = '+';
+		s++;
+		}
+		break;
+	default:
+		ret = 'a';
+		while(s < es && !strchr(SPACES, *s) && !strchr(SYMBOLS, *s))
+		s++;
+		break;
+	}
+	if(eq)
+		*eq = s;
+
+	while(s < es && strchr(SPACES, *s))
+		s++;
+	*ps = s;
+	return ret;
+}
