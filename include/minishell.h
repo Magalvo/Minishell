08ec3018d1d2ab082ee38f263fd8411ee3c4a8fc
kajo@42 <kajo@42>
2024-06-17 15:52:08 +0100

    more ft_getpid changes (final)

--- include/minishell.h
+++ include/minishell.h
@@ -118,9 +118,8 @@ char	**env_convert(t_env *env);
 void	free_env_list(t_env *env);
 void 	cleanup_shell(t_ms *s);
 void	clear_cmd(t_cmd *cmd);
-char	*get_pid(t_ms *s);
-char	*ft_get_pid(void);
-// void	set_pid(t_pid pid);
+// char	*get_pid(t_ms *s);
+char	*ft_getpid(void);
 
 /*
 ll_inits.c
