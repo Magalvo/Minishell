084a76796fa57917e4900f4c16e8d00e8410904b
kajo@42 <kajo@42>
2024-06-27 15:05:59 +0100

    norminette 2

--- include/minishell.h
+++ include/minishell.h
@@ -161,6 +161,7 @@ char	*get_env_expand(char *key);
 void	env_arr_update(t_ms *s, char *str);
 void	initialize_env(char ***envp);
 char	**null_env_init();
+void	free_and_error(char *one, char *two, char **three);
 char	**env_convert(t_env *env);
 char	**create_env_array(t_env *env, int *ctd);
 void	free_env_list(t_env *env);
@@ -186,19 +187,21 @@ char	*join_key_value(const char *key, const char *value);
 
 //*================= EXEC =========================*//
 
-//todo int		cmd_exec(char *args);
-//int				exec_input(t_ms *s);
-//char			*search_path(char *command, char **paths);
-char			*env_paths(t_ms *ms, char **envp);
-char			*cmd_path(char **paths, char *cmd, t_ms *s);
-void			exec_from_ast(t_ms *s);
-void			exec_pipe(t_ms *s, t_cmd *cmd, int fd_in, int fd_out);
-void			exec_one(t_ms *s, char **argv);
-void			single_exec(t_ms *s, t_cmd *cmd, int fd_in, int fd_out);
-void			exec_redir(t_ms *s, t_cmd *cmd, int fd_in, int fd_out);
-void			exec_from_ast_recursive(t_ms *s, t_cmd *cmd, int fd_in, int fd_out);
-void			updating_cmds(t_ms *s, char *value);
-void			aux_rec_exec(t_ms *s, t_cmd *cmd, int fd_in, int fd_out);
+//todo int	cmd_exec(char *args);
+//int			exec_input(t_ms *s);
+//char		*search_path(char *command, char **paths);
+char		*env_paths(t_ms *ms, char **envp);
+char		*cmd_path(char **paths, char *cmd, t_ms *s);
+void		exec_from_ast(t_ms *s);
+void		exec_pipe(t_ms *s, t_cmd *cmd, int fd_in, int fd_out);
+void		exec_one(t_ms *s, char **argv);
+void		single_exec(t_ms *s, t_cmd *cmd, int fd_in, int fd_out);
+void		exec_redir(t_ms *s, t_cmd *cmd, int fd_in, int fd_out);
+void		exec_redir_fork(t_ms *s, t_cmd *cmd, int fd_in, int fd_out);
+void		close_two_fd(t_cmd *cmd, int fd_in, int fd_out);
+void		exec_from_ast_recursive(t_ms *s, t_cmd *cmd, int fd_in, int fd_out);
+void		updating_cmds(t_ms *s, char *value);
+void		aux_rec_exec(t_ms *s, t_cmd *cmd, int fd_in, int fd_out);
 
 //*================= ERRORS =========================*//
 
